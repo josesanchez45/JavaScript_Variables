18bb51b806da9205ad205dd8436e1dd01b0f570b
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-14 12:32:41 -0700

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,69 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jose";
+
+let lastName = "S";
+
+var age = 50;
+
+console.log(`${firstName}`, `${lastName}`, `${age}`);
 
 // Exercise 2
+//firstName = "Jose";
+
+lastName = "Sanchez";
+
+age = 32;
+
+console.log(`${lastName}`, `${firstName}`, `${age}`);
 
 // Exercise 3
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+console.log(`${language}`, `${createdYear}`, `${isCaseSensitive}`);
+
+//Exercise 4
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+console.log(
+  `${price}`,
+  `${isOnSale}`,
+  `${salePercentage}`,
+  `${stock}`,
+  `${inStock}`,
+  `${selectedSize}`
+);
+
+//Exercise 5
+let title = "Name of Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
+console.log(
+  `${title}`,
+  `${author}`,
+  `${pageCount}`,
+  `${bookmark}`,
+  `${hasRead}`
+);
